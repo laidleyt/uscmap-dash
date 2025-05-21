6e6053ab3e0b0f2765dc0bb62e96b5e6d36ff741
laidleyt <tom.laidley@gmail.com>
2025-05-21 10:24:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -261,4 +261,5 @@ def toggle_about(n_clicks, current_style):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8050)))
